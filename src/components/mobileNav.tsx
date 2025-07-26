04744b0eedd48a72d00f29d9b547c20946b9242e
Sylvia Crowe <software@oneirocosm.com>
2025-07-25 18:26:40 -0700

    build: bump astro to 5.2 and tailwind to 4

--- src/components/mobileNav.tsx
+++ src/components/mobileNav.tsx
@@ -53,7 +53,7 @@ export function MobileNav() {
                       <li key={item.href}>
                         <a
                           href={item.href}
-                          className={`block select-none space-y-1 rounded-md p-3 leading-none no-underline outline-none transition-colors hover:bg-accent hover:text-accent-foreground focus:bg-accent focus:text-accent-foreground ${
+                          className={`block select-none space-y-1 rounded-md p-3 leading-none no-underline outline-hidden transition-colors hover:bg-accent hover:text-accent-foreground focus:bg-accent focus:text-accent-foreground ${
                             activePath === item.href
                               ? "bg-accent text-accent-foreground"
                               : ""
--- src/components/theme.tsx
+++ src/components/theme.tsx
@@ -55,13 +55,13 @@ export function ThemeToggle() {
           setVisible(true);
         }}
       >
-        <Sun className="h-[1.5rem] w-[1.3rem] dark:hidden sicko:hidden" />
+        <Sun className="h-6 w-[1.3rem] dark:hidden sicko:hidden" />
         <Moon className="hidden h-5 w-5 dark:block sicko:hidden" />
     <svg
       viewBox="0 0 512 512"
       width={160}
       height={160}
-      className="mx-auto h-[1.5rem] w-[1.3rem] fill-current hidden sicko:block">
+      className="mx-auto h-6 w-[1.3rem] fill-current hidden sicko:block">
       <path d="M464 256A208 208 0 1 0 48 256a208 208 0 1 0 416 0zM0 256a256 256 0 1 1 512 0A256 256 0 1 1 0 256zm256 32a64 64 0 1 1 0 128 64 64 0 1 1 0-128zM103 135c9.4-9.4 24.6-9.4 33.9 0l23 23 23-23c9.4-9.4 24.6-9.4 33.9 0s9.4 24.6 0 33.9l-23 23 23 23c9.4 9.4 9.4 24.6 0 33.9s-24.6 9.4-33.9 0l-23-23-23 23c-9.4 9.4-24.6 9.4-33.9 0s-9.4-24.6 0-33.9l23-23-23-23c-9.4-9.4-9.4-24.6 0-33.9zm192 0c9.4-9.4 24.6-9.4 33.9 0l23 23 23-23c9.4-9.4 24.6-9.4 33.9 0s9.4 24.6 0 33.9l-23 23 23 23c9.4 9.4 9.4 24.6 0 33.9s-24.6 9.4-33.9 0l-23-23-23 23c-9.4 9.4-24.6 9.4-33.9 0s-9.4-24.6 0-33.9l23-23-23-23c-9.4-9.4-9.4-24.6 0-33.9z"/>
     </svg>
         <span className="sr-only">Toggle theme</span>
